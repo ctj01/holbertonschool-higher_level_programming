414a5b9b15898a8b2ae80bbca0ddc65de7a6a618
ctj01 <flexdancer507@gmail.com>
2021-01-20 07:00:02 -0500

    adding

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -1,5 +1,20 @@
 #!/usr/bin/python3
+"""
+add_atrribute definition
+"""
+
+
 def add_attribute(obj, name, value):
+    """[summary]
+
+    Args:
+        obj ([type]): [description]
+        name ([type]): [description]
+        value ([type]): [description]
+
+    Raises:
+        TypeError: [description]
+    """
     if hasattr(obj, '__dict__') is False:
         raise TypeError("can't add new attribute")
     else:
